97eda238a64e9f96b70c14291e0fc97152874fd9
José Lucas Silva Mayer <josemayer@usp.br>
2022-06-08 10:48:21 -0300

    [1] Add single value square test case
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- test_magicSquare.py
+++ test_magicSquare.py
@@ -6,6 +6,10 @@ class Test(unittest.TestCase):
         self.square1 = [[1, 2, 3],
                         [4, 5, 6],
                         [7, 8, 9]]
+        self.square2 = [[1, 1, 1],
+                        [1, 1, 1],
+                        [1, 1, 1]]
 
     def test_magicSquare(self):
         self.assertEqual(magicSquare(self.square1), False)
+        self.assertEqual(magicSquare(self.square2), True)
