78893ec07665059c56585d7cb6f10c6b64fba19d
José Lucas Silva Mayer <josemayer@usp.br>
2022-06-08 10:54:42 -0300

    [2] Update function to verify single values squares
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- magicSquare.py
+++ magicSquare.py
@@ -1,2 +1,7 @@
 def magicSquare(square):
-    return False;
+    value = square[0][0]
+    for line in square:
+        for element in line:
+            if element != value:
+                return False
+    return True;
